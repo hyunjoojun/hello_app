a551707f2b428a2f893bad8088f2be77bb938491
Hyunjoo Jun <hjj102088@gmail.com>
2022-01-31 20:49:25 -0500

    Added rails scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#goodbye'
+  root 'users#index'
   get "/hello_world", to: "application#hello"
   get "/goodbye_world", to: "application#goodbye"
 end
